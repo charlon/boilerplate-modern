a7abda77a1708e50ec7090465bf7d7f75e31c1b0
Charlon Palacay <charlon@gmail.com>
2018-08-23 22:35:37 -0700

    Fixes outer Bootstrap containers and general styling of boilerplate.

--- boilerplate/static/boilerplate/js/utilities.js
+++ boilerplate/static/boilerplate/js/utilities.js
@@ -1,5 +1,5 @@
 // class toggler
 export function toggleClass() {
     var element = document.getElementById("nav_menu");
-    element.classList.toggle("bp-slide-down");
+    element.classList.toggle("u-slide-down");
 }
