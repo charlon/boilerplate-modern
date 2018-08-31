22586a0876c77f971843f68579387e663ae4d7c0
Charlon Palacay <charlon@gmail.com>
2018-08-31 15:56:56 -0700

    Trying to get typescript json data to display.

--- myuw/static/home/AcademicCalendar.tsx
+++ myuw/static/home/AcademicCalendar.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {
-  Card, CardText, CardBody,
+  Card, CardBody,
   CardTitle, CardSubtitle
 } from 'reactstrap';
 
@@ -26,20 +26,44 @@ class AcademicCalendar extends React.Component<Props, State> {
 
   public componentDidMount() {
 
+    // use native fetch to consume local json api
     fetch('/calendar.json')
-    .then(results => {
-        return results.json();
-      }).then(data => {
-        this.setState({
-          events: data,
-          loading : false
-        });
-        console.log(this.state.loading);
-        console.log(this.state.events);
+    .then(results => { return results.json() })
+    .then(data => {
+
+      this.setState({
+        events: data,
+        loading : false
       });
+
+      console.log(this.state.loading);
+      console.log(this.state.events);
+
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   }
+  // https://github.com/Lemoncode/react-typescript-samples/tree/master/06%20AJAX%20Call
+  // example typescript
 
   public render() {
+
+    let content;
+
+    if (this.state.loading) {
+      content = <div>Loading...</div>;
+    } else {
+      content =
+      <ul>
+        {this.state.events.map(event => {
+          return (
+            <li>asfddasasd</li>
+          );
+        })}
+      </ul>
+    }
 
     return (
       <div>
@@ -47,7 +71,7 @@ class AcademicCalendar extends React.Component<Props, State> {
           <CardBody>
             <CardTitle>Academic Calender</CardTitle>
             <CardSubtitle>Hello from {this.props.compiler} and {this.props.framework}!</CardSubtitle>
-            <CardText>asfdasd asdfasdfas</CardText>
+            <div>{content}</div>
           </CardBody>
         </Card>
       </div>
