07ebd18aa2d00d54d183ab2cb09c5c36a546e78a
Charlon Palacay <charlon@gmail.com>
2018-09-13 14:50:58 -0700

    Removes unused css files.

--- boilerplate-ts/src/App.tsx
+++ boilerplate-ts/src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Route, Switch} from 'react-router-dom';
 
-import './App.css';
 import Boilerplate from './boilerplate/Boilerplate';
 import Home from './pages/home/Home'
 import Teaching from './pages/Teaching'
